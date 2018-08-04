a0035b3646edafe0616fa7924c6e4fdab18c06b6
John Hinnegan <github-commit@johnhinnegan.com>
2018-08-04 16:44:02 -0400

    notes on scripting

--- fork_scripts.rb
+++ fork_scripts.rb
@@ -6,3 +6,10 @@ github = Github.new
 github.repos.list(org: 'goldencorgy').each do |repo|
   puts repo
 end
+
+# github.repos.list(org: 'goldencorgy', auto_pagination: true).each do |repo| 
+#   puts "forking #{repo['name']}" 
+#   github.repos.forks.create("goldencorgy", repo["name"], user: "softwaregravy") rescue nil
+# end 
+#
+# github = Github.new basic_auth: "not my auth token"
